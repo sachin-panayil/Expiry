58ea2aec32e01e4b9d821a92d3f186c48a13a4f0
Sachin Panayil <sachinpanayil01@gmail.com>
2021-04-28 19:34:12 -0400

    Initial Commit

---
+++ ExpiryTests/ExpiryTests.swift
@@ -0,0 +1,33 @@
+//
+//  ExpiryTests.swift
+//  ExpiryTests
+//
+//  Created by Sachin Panayil on 4/28/21.
+//
+
+import XCTest
+@testable import Expiry
+
+class ExpiryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
